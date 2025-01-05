8808b5ffec14200fe54e6800e8bb0ef533c25c01
RedwanParvez100 <19202103100@cse.bubt.edu.bd>
2025-01-05 14:56:27 +0600

    product page full functionality added

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,12 +2,14 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
     const { productId } = useParams();
     const { products, currency } = useContext(ShopContext);
     const [productData, setProductData] = useState(false);
     const [image, setImage] = useState("");
+    const [size, SetSize] = useState("");
 
     const fetchProductData = async () => {
         products.map((item) => {
@@ -70,8 +72,60 @@ const Product = () => {
                     <p className="mt-5 text-gray-500 md:w-4/5">
                         {productData.description}
                     </p>
+                    <div className="flex flex-col gap-4 my-8">
+                        <p>Select Size</p>
+                        <div className="flex gap-2">
+                            {productData.sizes.map((item, index) => (
+                                <button
+                                    onClick={() => SetSize(item)}
+                                    className={`border py-2 px-4 bg-gray-100 ${
+                                        item === size ? "border-orange-500" : ""
+                                    }`}
+                                    key={index}
+                                >
+                                    {item}
+                                </button>
+                            ))}
+                        </div>
+                    </div>
+                    <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+                        ADD TO CART
+                    </button>
+                    <hr className="mt-8 sm:w-4/5" />
+                    <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+                        <p>100% Original Product</p>
+                        <p>Cash on delivery is available on this product</p>
+                        <p>Easy return & exchange policy within 7 days</p>
+                    </div>
                 </div>
             </div>
+
+            {/* ----- Description & Review Section */}
+            <div className="mt-20">
+                <div className="flex">
+                    <b className="border px-5 py-3 text-sm">Description</b>
+                    <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+                </div>
+                <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Accusantium veniam vel quia velit consequuntur, debitis
+                        laudantium, dolor eaque nesciunt
+                    </p>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Placeat vel rerum culpa nisi expedita iure
+                        accusantium omnis deleniti, commodi unde nam sed quae
+                        eum? Numquam sequi quasi laboriosam corporis placeat.
+                    </p>
+                </div>
+            </div>
+
+            {/* ------ Display related products -------- */}
+            <RelatedProducts
+                category={productData.category}
+                subCategory={productData.subCategory}
+            />
         </div>
     ) : (
         <div className="opacity-0"></div>
