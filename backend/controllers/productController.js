8b003b56d4921753da9497366df6cc1a2199acd2
RedwanParvez100 <19202103100@cse.bubt.edu.bd>
2025-01-18 12:31:17 +0600

    product save in db, added product display in frontend ongoing

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,6 @@
+import { v2 as cloudinary } from "cloudinary";
+import productModel from "../models/productModel.js";
+
 // function for add product
 const addProduct = async (req, res) => {
     try {
@@ -16,6 +19,21 @@ const addProduct = async (req, res) => {
         const image3 = req.files.image3 && req.files.image3[0];
         const image4 = req.files.image4 && req.files.image4[0];
 
+        const images = [image1, image2, image3, image4].filter(
+            (item) => item !== undefined
+        );
+
+        let imagesUrl = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path, {
+                    resource_type: "image",
+                });
+                return result.secure_url;
+            })
+        );
+
+        /*  all images url process
+
         console.log(
             name,
             description,
@@ -28,9 +46,28 @@ const addProduct = async (req, res) => {
 
         // console.log(req.files);
 
-        console.log(image1, image2, image3, image4);
+        console.log(imagesUrl);
 
-        res.json({});
+        */
+
+        const productData = {
+            name,
+            description,
+            category,
+            price: Number(price),
+            subCategory,
+            bestseller: bestseller === "true" ? true : false,
+            sizes: JSON.parse(sizes),
+            image: imagesUrl,
+            date: Date.now(),
+        };
+
+        console.log(productData);
+
+        const product = new productModel(productData);
+        await product.save();
+
+        res.json({ success: true, message: "Product Added" });
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: error.message });
