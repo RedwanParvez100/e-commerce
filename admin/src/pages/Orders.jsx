38e0ea000cff4b4205e7fd8f49d59d159cd33084
RedwanParvez100 <19202103100@cse.bubt.edu.bd>
2025-04-01 19:13:00 +0600

    status update functionality added

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -28,6 +28,22 @@ const Orders = ({ token }) => {
         }
     };
 
+    const statusHandler = async (event, orderId) => {
+        try {
+            const response = await axios.post(
+                backendUrl + "/api/order/status",
+                { orderId, status: event.target.value },
+                { headers: { token } }
+            );
+            if (response.data.success) {
+                await fetchAllOrders();
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(response.data.message);
+        }
+    };
+
     useEffect(() => {
         fetchAllOrders();
     }, [token]);
@@ -37,21 +53,24 @@ const Orders = ({ token }) => {
             <h3>Order Page</h3>
             <div>
                 {orders.map((order, index) => (
-                    <div key={index}>
-                        <img src={assets.parcel_icon} alt="" />
+                    <div
+                        className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700"
+                        key={index}
+                    >
+                        <img className="w-12" src={assets.parcel_icon} alt="" />
                         <div>
                             <div>
                                 {order.items.map((item, index) => {
                                     if (index === order.items.length - 1) {
                                         return (
-                                            <p key={index}>
+                                            <p className="py-0.5" key={index}>
                                                 {item.name} x {item.quantity}
                                                 <span>{item.size}</span>
                                             </p>
                                         );
                                     } else {
                                         return (
-                                            <p key={index}>
+                                            <p className="py-0.5" key={index}>
                                                 {item.name} x {item.quantity}
                                                 <span>{item.size}</span> ,
                                             </p>
@@ -59,7 +78,7 @@ const Orders = ({ token }) => {
                                     }
                                 })}
                             </div>
-                            <p>
+                            <p className="mt-3 mb-2 font-medium">
                                 {order.address.firstName +
                                     " " +
                                     order.address.lastName}
@@ -79,8 +98,12 @@ const Orders = ({ token }) => {
                             <p>{order.address.phone}</p>
                         </div>
                         <div>
-                            <p>Items : {order.items.length}</p>
-                            <p>Method : {order.paymentMethod}</p>
+                            <p className="text-sm sm:text-[15px]">
+                                Items : {order.items.length}
+                            </p>
+                            <p className="mt-3">
+                                Method : {order.paymentMethod}
+                            </p>
                             <p>
                                 Payment : {order.payment ? "Done" : "Pending"}
                             </p>
@@ -89,11 +112,17 @@ const Orders = ({ token }) => {
                                 {new Date(order.date).toLocaleDateString()}
                             </p>
                         </div>
-                        <p>
+                        <p className="text-sm sm:text-[15px]">
                             {currency}
                             {order.amount}
                         </p>
-                        <select>
+                        <select
+                            onChange={(event) =>
+                                statusHandler(event, order._id)
+                            }
+                            value={order.status}
+                            className="p-2 font-semibold"
+                        >
                             <option value="Order Placed">Order Placed</option>
                             <option value="Packing">Packing</option>
                             <option value="Shipped">Shipped</option>
